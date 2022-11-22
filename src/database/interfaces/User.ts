fe4dd67561fe168e07cdba7c25a9cbd65edc41a5
sadarocco <sadarocco@gmail.com>
2022-11-22 08:37:31 -0300

    delete moved assets

--- src/database/interfaces/User.ts
+++
@@ -1,8 +0,0 @@
-export type UserData = {
-  id: string
-  firstName: string
-  lastName?: string
-  email: string
-  password: string
-  dateOfBirth?: string
-}
--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import userController from './../controller/user'
-
-const router = express.Router()
-
-router.post('/user', userController.createUser)
-router.get('/user/:id', userController.getUser)
-
-export default router
